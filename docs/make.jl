13b9dbb74ce8f1c7cde1ddc18a49f44d1df9657e
cfranken <cfranken@curry.gps.caltech.edu>
2021-04-28 13:13:47 -0700

    Add tutorial

--- docs/make.jl
+++ docs/make.jl
@@ -1,31 +1,45 @@
-push!(LOAD_PATH,"./src/")
+push!(LOAD_PATH,joinpath(@__DIR__, "../src"))
 
-using Documenter, InstrumentOperator
+using Documenter, InstrumentOperator, Literate, Plots
 
-pages = Any[
-        "Home"                  => "index.md"
-    ]
+function build()
 
-mathengine = MathJax(Dict(
-        :TeX => Dict(
-            :equationNumbers => Dict(:autoNumber => "AMS"),
-            :Macros => Dict(),
-        ),
-    ))
+    tutorials = ["CrossSection_convolution.jl"] # , 
+    tutorials_paths = [joinpath(@__DIR__, "src", "pages", "tutorials", tutorial) for tutorial in tutorials]
 
- format = Documenter.HTML(
-        prettyurls = get(ENV, "CI", nothing) == "true",
-        mathengine = mathengine,
-        collapselevel = 1,
-    )
+    for tutorial in tutorials_paths
+        Literate.markdown(tutorial, joinpath(@__DIR__, "src", "pages", "tutorials"))
+    end
+    tutorials_md = [joinpath("pages", "tutorials", tutorial[1:end-3]) * ".md" for tutorial in tutorials]
 
-makedocs(
+    pages = Any[
+            "Home"                  => "index.md",
+            "Tutorials"             => tutorials_md
+        ]
+
+    mathengine = MathJax(Dict(
+            :TeX => Dict(
+                :equationNumbers => Dict(:autoNumber => "AMS"),
+                :Macros => Dict(),
+            ),
+        ))
+
+    format = Documenter.HTML(
+            prettyurls = get(ENV, "CI", nothing) == "true",
+            mathengine = mathengine,
+            collapselevel = 1,
+        )
+    
+    makedocs(
     sitename = "Instrument Operator",
     format = format,
     clean = false,
     modules = [InstrumentOperator],
     pages = pages
 )
+end
+
+build()
 
 deploydocs(
     repo = "github.com/RadiativeTransfer/InstrumentOperator.jl.git",
