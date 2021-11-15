2b71474a08e4b4c6cd1f512bc9f722cc0cd7fdd7
cfranken <fronge@gmail.com>
2021-11-15 05:35:04 -0800

    Add L1 reader cababilities (half way through)

--- src/io_L1_files.jl
+++ src/io_L1_files.jl
@@ -4,7 +4,7 @@
 # ocoData = Dataset(oco_file)
 # geometry, ils, meteo, measurement = load_L1(dict,ocoData, met)
 
-function load_L1(dict, L1, met)
+function load_L1(dict, L1::NCDataset, met::NCDataset)
     # Dicts with mostly NCVars but also some basic stuff:
     geometry    = Dict{String, Any}()
     ils         = Dict{String, Any}()
@@ -23,7 +23,17 @@ function load_L1(dict, L1, met)
     # Reading in Meausurements
     loadEntries!(L1, dict, ils, "ILS")
 
-    return geometry, ils, meteo, measurement
+    return L1_OCO(geometry, ils, meteo, measurement)
+end
+
+function load_L1(dict::OrderedDict, L1::String, met::String)
+    ocoData = Dataset(L1);    
+    metData = Dataset(met);
+    # Load dictionary:
+    dictOCO2 = YAML.load_file(dict; dicttype=OrderedDict{String,Any});
+
+    # Load L1 file (could just use filenames here as well later on)
+    return load_L1(dictOCO2,ocoData, metData)
 end
 
 function loadEntries!(NC, dict, out_dict, name)
@@ -36,5 +46,43 @@ function loadEntries!(NC, dict, out_dict, name)
             end
         end
     end
+end
+
+function getMeasurement(oco::L1_OCO, bands::Tuple, indices::Tuple, GeoInd)
+    @assert length(indices) == length(bands) "Length of bands and indices has to be identical"
+    n = length(indices)
+    # First band:
+    band = oco.measurement["bands"][bands[1]]
+    # Extended Dimensions (irrespective of sounding)
+    extended_dims = [GeoInd[1],bands[1]];
+    ind  = indices[1]
+    bandIndices = (ind .- ind[1] .+ 1,)
+    rad = oco.measurement[band][ind,GeoInd...]
+    dispPoly = Polynomial(view(oco.ils["dispersion"], :, extended_dims...))
+    ν = (dispPoly.(indices[1]))
+
+    # Concatenate rest (if applicable)
+    for i=2:n
+        band = oco.measurement["bands"][bands[i]]
+        ind  = indices[i]
+        bandIndices = (bandIndices..., ind .-ind[1] .+ 1 .+ bandIndices[end][end])
+        rad = [rad; oco.measurement[band][ind,GeoInd...]]
+        extended_dims = [GeoInd[1],bands[i]];
+        dispPoly = Polynomial(view(oco.ils["dispersion"], :, extended_dims...))
+        ν = [ν; (dispPoly.(indices[i]))]
+    end
+
+    #### Meteo stuff  ###
+    p_half = reverse(oco.meteo["ak"] + oco.meteo["bk"] * oco.meteo["p_surf"][GeoInd...])/100
+    T   =    oco.meteo["T"][:,GeoInd...] 
+    q   =    oco.meteo["q"][:,GeoInd...]
+    lat = oco.geometry["lat"][GeoInd...]
+    lon = oco.geometry["lon"][GeoInd...]
+    sza = oco.geometry["sza"][GeoInd...]
+    ϕ   = oco.geometry["ϕ"][GeoInd...]
+    vza = oco.geometry["vza"][GeoInd...]
+    azi = oco.geometry["azi"][GeoInd...]
+
+    return ν, rad, bandIndices, p_half, T, q
 end
 
---
+++ test/oco_file_reader_test.jl
@@ -0,0 +1,27 @@
+using NCDatasets
+using OrderedCollections
+using YAML
+
+# Load L1 file (replace with your example!):
+L1File = "/net/fluo/data1/group/oco2/L1bSc/oco2_L1bScND_26780a_190715_B10003r_200429212407.h5"
+ocoData = Dataset(L1File);
+
+# Load corresponding met file:
+metFile = "/net/fluo/data1/group/oco2/L2Met/oco2_L2MetND_26780a_190715_B10003r_200429212406.h5"
+metData = Dataset(metFile);
+
+# Load dictionary:
+dictOCO2 = YAML.load_file("json/oco2.yaml"; dicttype=OrderedDict{String,Any});
+
+# Load L1 file (could just use filenames here as well)
+oco = InstrumentOperator.load_L1(dictOCO2,ocoData, metData);
+
+# Pick some bands as tuple (or just one)
+bands = (1,2);
+# Indices within that band:
+indices = (1:1016,20:30);
+# Geo Index (footprint,sounding):
+GeoInd = [5,4000];
+
+# Get data for that sounding:
+ν, a, b, p, T, q = InstrumentOperator.getMeasurement(oco, bands, indices, GeoInd)
